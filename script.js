ba82be5b7c20cee8c6ee37eee69bce9d1f193e9e
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-02-04 21:12:29 +0400

    working codes with correct output

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let playerScore = 0;
 let computerScore = 0;
 let message = "";
+let playerWins = 0;
+let computerWins = 0;
+let again = false;
 
 function computerPlay() {
   const choices = ["Rock", "Paper", "Scissor"];
@@ -11,6 +14,8 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
+    playerScore;
+    computerScore;
     return `It's a Tie, You both picked ${playerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "scissor") {
     playerScore++;
@@ -34,36 +39,86 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     const computerSelection = computerPlay();
     let playerSelection = prompt("Rock, Paper or Scissors")
       .toLowerCase()
       .replace(/\s+/g, "");
-    if (playerSelection === "rocks") {
+
+    if (playerSelection === "rocks" || playerSelection === "rock") {
       playerSelection = "rock";
-    } else if (playerSelection === "papers") {
+      console.log(`Round Number: ${i}
+      Player selection is: ${playerSelection}
+      Computer selection is: ${computerSelection}`);
+      //playRound(playerSelection, computerSelection);
+      console.log(playRound(playerSelection, computerSelection));
+    } else if (playerSelection === "papers" || playerSelection === "paper") {
       playerSelection = "paper";
-    } else if (playerSelection === "scissors") {
+      console.log(`Round Number: ${i}
+      Player selection is: ${playerSelection}
+      Computer selection is: ${computerSelection}`);
+      //playRound(playerSelection, computerSelection);
+      console.log(playRound(playerSelection, computerSelection));
+    } else if (
+      playerSelection === "scissors" ||
+      playerSelection === "scissor"
+    ) {
       playerSelection = "scissor";
+      console.log(`Round Number: ${i}
+      Player selection is: ${playerSelection}
+      Computer selection is: ${computerSelection}`);
+      //playRound(playerSelection, computerSelection);
+      console.log(playRound(playerSelection, computerSelection));
+    } else {
+      console.log("Invalid Entry");
+      i--;
     }
-    console.log(`Player selection is: ${playerSelection} `);
-    console.log(`Computer selection is: ${computerSelection} `);
-    console.log(playRound(playerSelection, computerSelection));
   }
+  scoreCard();
+  //Reports the winner
+  // console.log("Game is over \nResult: " + message);
+  // alert(
+  //   `Thank You for playing! \nThe score is: \nPlayer: ${playerScore} - Computer: ${computerScore} \nTotal Wins by Player: ${playerWins} \nTotal Wins by Computer: ${computerWins} \n${message}`
+  // );
+  playerScore = 0;
+  computerScore = 0;
+}
+
+//debugger;
+
+const playRequest = function () {
+  if (playAgain()) {
+    return game();
+  } else {
+    alert(
+      `Thank You for playing! \nThe Round score is: \nPlayer: ${playerScore} - Computer: ${computerScore} \nTotal Wins by Player: ${playerWins} \nTotal Wins by Computer: ${computerWins} \n${message}`
+    );
+  }
+};
+
+function scoreCard() {
   // determine winner of 5 round game
   if (playerScore > computerScore) {
     message = "Congratulations! You won!!!";
+    console.log(message);
+    playerWins++;
+    playRequest();
   } else if (playerScore === computerScore) {
-    message = "You tied with the Computer. Try Again!";
+    message = `You tied with the Computer. Try Again!
+    Player Wins Total: ${playerWins}
+    Computer Wins Total: ${computerWins}`;
+    console.log(message);
+    playRequest();
   } else {
     message = "Loser! Computer wins the game ;(";
+    console.log(message);
+    computerWins++;
+    playRequest();
   }
-  // Reports the winner
-  console.log("Game is over \nResult: " + message);
-  alert(
-    `Thank You for playing! \nThe score is: \nPlayer: ${playerScore} - Computer: ${computerScore} \n${message}`
-  );
 }
 
+const playAgain = function () {
+  return window.confirm("Do you want to play another round?");
+};
 // Starts the game
 game();
